55be881e7df9b52b60fb7cb48e626dfc2dc80402
rup-arc <rd8851188@gmail.com>
2025-07-30 18:19:38 +0530

    Update app.py
    cioommit

--- app/app.py
+++ app/app.py
@@ -20,8 +20,8 @@ cursor = db.cursor()
 # S3
 s3 = boto3.client(
     's3',
-    aws_access_key_id=os.environ['AWS_ACCESS_KEY'],
-    aws_secret_access_key=os.environ['AWS_SECRET_KEY']
+    aws_access_key_id=os.environ['AKIA5JMST4MTJGBRFEGW'],
+    aws_secret_access_key=os.environ['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']
 )
 BUCKET = os.environ['S3_BUCKET']
 
